81ca0cac7d22632a7f1bc434444de1e1917a1669
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    X00TD: Build separate system and vendor hidl lib

--- device.mk
+++ device.mk
@@ -245,7 +245,7 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0_system
 
 # HW Crypto
 PRODUCT_PACKAGES += \
