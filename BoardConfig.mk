9bdf8a828e83bc95b5a5b49bfb99c05057ab42a3
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-13 16:41:34 -0300

    X00TD: Enable TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,6 +203,9 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
+# Telephony
+TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
+
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true
 
