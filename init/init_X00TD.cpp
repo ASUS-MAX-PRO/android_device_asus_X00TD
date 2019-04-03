352eb3a7927601706a928c4def6288f80add7c2e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-04-03 10:32:25 +0530

    X00TD: Use pixel fp to bypass safetynet

--- init/init_X00TD.cpp
+++ init/init_X00TD.cpp
@@ -132,12 +132,10 @@ void vendor_check_variant()
     if (sys.totalram > 4096ull * 1024 * 1024) {
         // Russian model
         if (region == "RU") {
-            build_fingerprint = "asus/RU_X00TD/ASUS_X00T_9:9/PKQ1/16.2017.1912.060-20191125:user/release-keys";
             product_device = "ASUS_X00T_9";
 
         // Global model
         } else {
-            build_fingerprint = "asus/WW_X00TD/ASUS_X00T_3:9/PKQ1/16.2017.1912.060-20191125:user/release-keys";
             product_device = "ASUS_X00T_3";
         }
 
@@ -145,12 +143,10 @@ void vendor_check_variant()
     } else {
         // Russian model
         if (region == "RU") {
-            build_fingerprint = "asus/RU_X00TD/ASUS_X00T_6:9/PKQ1/16.2017.1912.060-20191125:user/release-keys";
             product_device = "ASUS_X00T_6";
 
         // Global model
         } else {
-            build_fingerprint = "asus/WW_X00TD/ASUS_X00T_2:9/PKQ1/16.2017.1912.060-20191125:user/release-keys";
             product_device = "ASUS_X00T_2";
         }
     }
@@ -163,6 +159,9 @@ void vendor_check_variant()
         product_model = "ASUS_X00TDA";
     else
         product_model = "ASUS_X00TD";
+
+    //Google walley Fingerptint
+    build_fingerprint = "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys";
 
     // Override props based on values set
     property_override_dual("ro.product.device", "ro.vendor.product.device", product_device);
