63adb2a56a24d4678ebadea76b5399bc5f001f68
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-11-21 09:36:40 +0700

    X00TD: set ro.telephony.iwlan_operation_mode to legacy
    since radio has been uprev to 1.4

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -200,6 +200,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=11 \
     ro.ril.ecclist=112,911 \
     ro.telephony.default_network=20,20 \
+    ro.telephony.iwlan_operation_mode=legacy \
     telephony.lteOnCdmaDevice=1
 
 # RmNet Data
