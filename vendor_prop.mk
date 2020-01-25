edcd2597f6717ef9c98eea63e3ab2534fdb265a2
dianlujitao <dianlujitao@lineageos.org>
2020-01-25 14:41:51 +0000

    X00TD: Tweak system props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -93,7 +93,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
     persist.vendor.btstack.enable.splita2dp=fales \
     ro.vendor.bluetooth.wipower=false \
-    vendor.bluetooth.soc=cherokee \
     vendor.qcom.bluetooth.soc=cherokee
 
 # Camera
@@ -102,9 +101,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.ois.disable=1 \
     persist.vendor.camera.is_type=4 \
     persist.ts.rtmakeup=1 \
-    persist.vendor.camera.expose.aux=1 \
-    persist.vendor.camera.preview.ubwc=0 \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam
+    persist.vendor.camera.expose.aux=1
 
 # Camera HAL Setup
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -132,10 +129,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
-# Enable stm-events
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.coresight.config=stm-events
-
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.fm.a2dp.conc.disabled=false \
@@ -147,35 +140,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_hwc_vds=1 \
-    debug.sf.early_phase_offset_ns=5000000 \
-    debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1 \
-    vendor.gralloc.enable_fb_ubwc=1 \
     debug.hwui.use_buffer_age=false \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     ro.opengles.version=196610 \
     sdm.debug.rotator_downscale=1 \
     vendor.display.enable_default_color_mode=1
-
-# Media
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.aac_51_output_enabled=true \
-    media.stagefright.enable-player=true \
-    media.stagefright.enable-http=true \
-    media.stagefright.enable-aac=true \
-    media.stagefright.enable-qcp=true \
-    media.stagefright.enable-scan=true \
-    mm.enable.smoothstreaming=true \
-    vendor.mm.enable.qcom_parser=13631487 \
-    mmp.enable.3g2=true \
-    persist.mm.enable.prefetch=true \
-    vendor.vidc.enc.disable_bframes=1 \
-    vendor.vidc.dec.enable.downscalar=1 \
-    vendor.vidc.enc.disable.pq=false \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    vidc.enc.target_support_bframe=1
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -186,31 +156,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     persist.vendor.qcomsysd.enabled=1
 
-# Netflix
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.netflix.bsp_rev=Q660-13149-1
-
-# Netmgr
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.use_data_netmgrd=true \
-    persist.vendor.data.mode=concurrent
-
-# Netflix
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.netflix.bsp_rev=Q660-13149-1
-
 # NFC
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.nfc_nci=pn8x \
-    ro.nfc.port=I2C
-
-# QCOM cabl
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.display.cabl=2
+    ro.hardware.nfc_nci=pn8x
 
 # QTI
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.at_library=libqti-at.so \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.qti.core_ctl_min_cpu=2 \
     ro.vendor.qti.core_ctl_max_cpu=4 \
@@ -221,26 +172,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
     persist.data.iwlan.enable=true \
     persist.radio.multisim.config=dsds \
-    persist.radio.VT_CAM_INTERFACE=1 \
-    persist.radio.VT_CAM_INTERFACE=2 \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.atfwd.start=true \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.qti.telephony.vt_cam_interface=1 \
-    ril.subscription.types=NV,RUIM \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
     persist.vendor.radio.procedure_bytes=SKIP \
     ro.carrier=unknown \
     ro.com.android.dataroaming=false \
     ro.config.vc_call_vol_steps=11 \
     ro.ril.ecclist=112,911 \
-    ro.telephony.default_network=20,20 \
-    telephony.lteOnCdmaDevice=1 \
     ro.telephony.iwlan_operation_mode=legacy \
     persist.vendor.radio.flexmap_type=none \
     persist.radio.aosp_usr_pref_sel=true \
+    persist.vendor.radio.add_power_save=1 \
     persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # VoLTE / VoWifi -Radio
@@ -249,12 +193,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
-    persist.radio.calls.on.ims=1
-    
-# RmNet Data
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.df.dev_name=rmnet_usb0 \
-    persist.vendor.radio.add_power_save=1
+    persist.radio.calls.on.ims=1    
 
 # Sensor
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -265,11 +204,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sensors.mot_detect=true \
     ro.vendor.qti.sensors.facing=false \
     ro.vendor.qti.sensors.cmc=false
-
-# Skip Validate Disable
-PRODUCT_PROPERTY_OVERRIDES += \
-    sdm.debug.disable_skip_validate=1 \
-    vendor.display.disable_skip_validate=1
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -295,12 +229,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # USB
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.usb.firstboot.config=diag,serial_cdev,rmnet,adb
-
-# WFD display
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.enable=1 \
-    persist.hwc.enable_vds=1 \
-    persist.sys.wfd.virtual=0
 
 # ZRAM disk
 PRODUCT_PROPERTY_OVERRIDES += \
