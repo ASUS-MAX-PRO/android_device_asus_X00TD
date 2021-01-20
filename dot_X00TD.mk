4541dd01b8b2d12ea2d0b7c94d9ecd701d89e974
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-01-20 11:39:52 +0700

    X00TD: fix bypass safety

--- dot_X00TD.mk
+++ dot_X00TD.mk
@@ -27,10 +27,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.210105.003 7005429 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_2:9/QKQ1/17.2017.2012.438-20201203:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
 
 # Set BootAnimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
